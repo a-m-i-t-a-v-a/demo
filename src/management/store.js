22ef970d6bdf14fb8ff3b41b3caa0ef31db56926
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-12 14:55:00 +0530

    Adding RTK Query logic

--- src/management/store.js
+++ src/management/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productSlice, { productsFetch } from "./productSlice";
+import productReducer, { productsFetch } from "./productSlice";
+import { productsApi } from "./productsApi";
 
 const store=configureStore({
     reducer:{
-        products:productSlice
-    }
+        products:productReducer,
+        [productsApi.reducerPath]:productsApi.reducer
+    },
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(productsApi.middleware)
+    
 })
 
 store.dispatch(productsFetch())
