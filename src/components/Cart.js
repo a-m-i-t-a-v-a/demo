54811017e5c52eeacf28d78b8a5e152fb4a8a6bb
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-13 14:32:14 +0530

    Cart funtionality and design added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
+
+  const cart=useSelector(store=>store.cart)
+
   return (
-    <div>
+    <div className='cart-container'>
       <h2>Cart</h2>
+      {cart.cartItems.length===0 ? (
+        <div className='cart-empty'>
+          <p>Your cart is empty</p>
+          <div className='start-shopping'>
+            <Link to='/'>
+              <span>Start Shopping</span>
+            </Link>
+          </div>
+        </div>
+      ):(
+        <div>
+          <div className='titles'>
+            <h3 className='product-title'>Product</h3>
+            <h3 className='price'>Price</h3>
+            <h3 className='quantity'>Quantity</h3>
+            <h3 className='total'>Total</h3>
+          </div>
+          <div className='cart-items'>
+            {cart.cartItems?.map(cartItem=>(
+              <div className='cart-item' key={cartItem.id}>
+                <div className='cart-product'>
+                  <img src={cartItem.image} alt={cartItem.name}/>
+                  <div>
+                    <h3>{cartItem.name}</h3>
+                    <p>{cartItem.description}</p>
+                    <button>Remove</button>
+                  </div>
+                </div>
+                <div className='cart-product-price'>
+                  ${cartItem.price}
+                </div>
+                <div className='cart-product-quantity'>
+                  <button>-</button>
+                  <div className='count'>{cartItem.cartQuantity}</div>
+                  <button>+</button>
+                </div>
+                <div className='cart-product-total-price'>
+                  ${cartItem.price*cartItem.cartQuantity}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='cart-summary'>
+            <button className='clear-cart'>Clear Cart</button>
+            <div className='cart-checkout'>
+              <div className='sub-total'>
+                <span>Sub total</span>
+                <span className='amount'>
+                  ${cart.cartTotalAmount}
+                </span>
+                <p>Taxes and Shipping calculated @ checkout</p>
+                <button>Check out</button>
+                <div className='continue-shopping'>
+                  <Link to='/'>
+                      <span>Continue Shopping</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
