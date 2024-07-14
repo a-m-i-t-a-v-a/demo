f0821213d704fe5de019b4d3dc34b9616848a203
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-14 23:57:53 +0530

    The button functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,86 +1,91 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { removeFromCart } from '../management/cartSlice'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { removeFromCart, decreaseCartQuantity, addToCart } from '../management/cartSlice';
 
 const Cart = () => {
+    const cart = useSelector(state => state.cart); 
+    const dispatch = useDispatch();
 
-  const cart=useSelector(store=>store.cart)
-  const dispatch=useDispatch()
+    const handleRemoveFromCart = (cartItem) => {
+        dispatch(removeFromCart(cartItem));
+    };
 
-  const handleRemoveFromCart=(cartItem)=>{
-    dispatch(removeFromCart(cartItem))
-  }
+    const handleCartItemDecreaseQuantity = (item) => {
+        dispatch(decreaseCartQuantity(item));
+    };
 
-  console.log(cart.cartItem)
+    const handleCartItemIncreaseQuantity = (item) => {
+        dispatch(addToCart(item));
+    };
 
-  return (
-    <div className='cart-container'>
-      <h2>Cart</h2>
-      {cart.cartItems.length===0 ? (
-        <div className='cart-empty'>
-          <p>Your cart is empty</p>
-          <div className='start-shopping'>
-            <Link to='/'>
-              <span>Start Shopping</span>
-            </Link>
-          </div>
+    return (
+        <div className='cart-container'>
+            <h2>Cart</h2>
+            {cart?.cartItems.length === 0 ? (
+                <div className='cart-empty'>
+                    <p>Your cart is empty</p>
+                    <div className='start-shopping'>
+                        <Link to='/'>
+                            <span>Start Shopping</span>
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <div className='titles'>
+                        <h3 className='product-title'>Product</h3>
+                        <h3 className='price'>Price</h3>
+                        <h3 className='quantity'>Quantity</h3>
+                        <h3 className='total'>Total</h3>
+                    </div>
+                    <div className='cart-items'>
+                        {cart.cartItems && cart.cartItems.map(cartItem => ( 
+                            <div className='cart-item' key={cartItem.id}>
+                                <div className='cart-product'>
+                                {cartItem.image && <img src={cartItem.image} alt={cartItem.name} />} 
+                                    <div>
+                                        <h3>{cartItem.name}</h3>
+                                        <p>{cartItem.description}</p>
+                                        <button onClick={() => handleRemoveFromCart(cartItem)}>Remove</button>
+                                    </div>
+                                </div>
+                                <div className='cart-product-price'>
+                                    ${cartItem.price}
+                                </div>
+                                <div className='cart-product-quantity'>
+                                    <button onClick={() => handleCartItemDecreaseQuantity(cartItem)}>-</button>
+                                    <div className='count'>{cartItem.cartQuantity}</div>
+                                    <button onClick={() => handleCartItemIncreaseQuantity(cartItem)}>+</button>
+                                </div>
+                                <div className='cart-product-total-price'>
+                                    ${cartItem.price * cartItem.cartQuantity}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='cart-summary'>
+                        <button className='clear-cart'>Clear Cart</button>
+                        <div className='cart-checkout'>
+                            <div className='sub-total'>
+                                <span>Sub total</span>
+                                <span className='amount'>
+                                    ${cart.cartTotalAmount}
+                                </span>
+                                <p>Taxes and Shipping calculated @ checkout</p>
+                                <button>Check out</button>
+                                <div className='continue-shopping'>
+                                    <Link to='/'>
+                                        <span>Continue Shopping</span>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-      ):(
-        <div>
-          <div className='titles'>
-            <h3 className='product-title'>Product</h3>
-            <h3 className='price'>Price</h3>
-            <h3 className='quantity'>Quantity</h3>
-            <h3 className='total'>Total</h3>
-          </div>
-          <div className='cart-items'>
-            {cart.cartItems?.map(cartItem=>(
-              <div className='cart-item' key={cartItem.id}>
-                <div className='cart-product'>
-                  <img src={cartItem.image} alt={cartItem.name}/>
-                  <div>
-                    <h3>{cartItem.name}</h3>
-                    <p>{cartItem.description}</p>
-                    <button onClick={()=>handleRemoveFromCart(cartItem)}>Remove</button>
-                  </div>
-                </div>
-                <div className='cart-product-price'>
-                  ${cartItem.price}
-                </div>
-                <div className='cart-product-quantity'>
-                  <button>-</button>
-                  <div className='count'>{cartItem.cartQuantity}</div>
-                  <button>+</button>
-                </div>
-                <div className='cart-product-total-price'>
-                  ${cartItem.price*cartItem.cartQuantity}
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className='cart-summary'>
-            <button className='clear-cart'>Clear Cart</button>
-            <div className='cart-checkout'>
-              <div className='sub-total'>
-                <span>Sub total</span>
-                <span className='amount'>
-                  ${cart.cartTotalAmount}
-                </span>
-                <p>Taxes and Shipping calculated @ checkout</p>
-                <button>Check out</button>
-                <div className='continue-shopping'>
-                  <Link to='/'>
-                      <span>Continue Shopping</span>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  )
-}
+    );
+};
 
-export default Cart
+export default Cart;
--- src/management/cartSlice.js
+++ src/management/cartSlice.js
@@ -10,7 +10,7 @@ const cartSlice=createSlice({
     },
     reducers:{
         addToCart(state,action){
-            const itemIndex=state.cartItems.findIndex(item=>item.id===action.payload.id)
+            const itemIndex=state.cartItems.findIndex(item=>item?.id===action.payload.id)
             if(itemIndex>=0){
                 state.cartItems[itemIndex].cartQuantity+=1;
                 toast.info(`Increased ${state.cartItems[itemIndex].name} product quantity`,{
@@ -26,17 +26,34 @@ const cartSlice=createSlice({
             localStorage.setItem("cartItems",JSON.stringify(state.cartItems))
         }, 
         removeFromCart(state,action){
-            const remainingCartItems=state.cartItems.filter((item)=>item.id!==action.payload.id)
+            const remainingCartItems=state.cartItems.filter((item)=>item?.id!==action.payload.id)
             state.cartItems=remainingCartItems;
 
             localStorage.setItem("cartItems",JSON.stringify(state.cartItems))
             toast.error(`${action.payload.name} removed from the cart`,{
                 position:"bottom-left"
             })
-        }
+        },
+        decreaseCartQuantity(state,action){
+            const itemIndex=state.cartItems.findIndex((cartItem)=>cartItem?.id===action.payload.id)
+            if(state.cartItems[itemIndex].cartQuantity>1){
+                state.cartItems[itemIndex].cartQuantity-=1
+                toast.info(`${action.payload.name} decreased from the cart`,{
+                    position:"bottom-left"
+                })
+            }else if(state.cartItems[itemIndex].cartQuantity===1){
+                const remainingCartItems=state.cartItems.filter((item)=>item?.id!==action.payload.id)
+                state.cartItems=remainingCartItems;
+
+                toast.error(`${action.payload.name} removed from the cart`,{
+                    position:"bottom-left"
+                })
+            }
+            localStorage.setItem("cartItems",JSON.stringify(state.cartItems))
+        },
     }
 })
 
-export const {addToCart,removeFromCart}=cartSlice.actions
+export const {addToCart,removeFromCart,decreaseCartQuantity}=cartSlice.actions
 
 export default cartSlice.reducer
